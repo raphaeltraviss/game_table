f185af4f0f4121dff175a3d78ff2a945ef37d3c0
Raphael Traviss <raphael@skyleafdesign.com>
2020-08-27 11:23:36 -0500

    AuthContainer inside stack nav

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AuthContainer from './container/AuthContainer';
+import MainContent from './container/MainContent';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 import tw from 'tailwind-rn';
 
 
+const Stack = createStackNavigator();
 
 export default function App() {
   const containerStyle = tw('flex-1 justify-center items-center bg-white');
 
   return (
     <NavigationContainer>
-      <View style={containerStyle}>
-        <AuthContainer />
-        <StatusBar style="auto" />
-      </View>
+          <Stack.Navigator initialRouteName="Auth">
+            <Stack.Screen name="Auth" component={AuthContainer} />
+            <Stack.Screen name="Main" component={MainContent} />
+          </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
---
+++ src/container/MainContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+export default function MainContent(props: any) {
+  return (
+    <View>
+      <Text>This is the main content</Text>
+    </View>
+  )
+}
