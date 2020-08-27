c3c56b9f56b7406bc9d05645a5a9213092df68c9
Raphael Traviss <raphael@skyleafdesign.com>
2020-08-27 11:11:20 -0500

    NavigationContainer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AuthContainer from './container/AuthContainer';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 import tw from 'tailwind-rn';
 
@@ -11,10 +13,12 @@ export default function App() {
   const containerStyle = tw('flex-1 justify-center items-center bg-white');
 
   return (
-    <View style={containerStyle}>
-      <AuthContainer />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={containerStyle}>
+        <AuthContainer />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
